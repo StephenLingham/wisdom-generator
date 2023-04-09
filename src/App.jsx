d52d89cdce9542c55d1d4bde2f5e4693d97fbdeb
Stephen Lingham <147378+StephenLingham@users.noreply.github.com>
2023-04-09 13:43:39 +0100

    Changed layout slightly

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 
 function App() {
   return (
     <div>
       <Container maxWidth="lg">
-        <Paper elevation={3} sx={{ padding: "20px", height: "50vh", marginTop: "20vh" }}>
-          <Stack 
-            spacing={13}
-            justifyContent="center"
-            alignItems="center"
-            sx={{height: "50vh"}}>
-            <Stack 
-              direction="row"
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          height="100vh">
+          <Paper
+            elevation={3}
+            sx={{ padding: "20px", height: "50vh" }}>
+            <Stack
+              spacing={13}
               justifyContent="center"
-              alignItems="center">
+              alignItems="center"
+              sx={{ height: "50vh" }}>
               <Button
                 variant="contained"
                 sx={{
@@ -29,12 +34,14 @@ function App() {
                 }}>
                 Generate Postmodern Wisdom
               </Button>
+              <Typography
+                variant="h2"
+                sx={{ textAlign: "center" }}>
+                Test text qwe qwe qwe qwe 123 123 123 123 123 123 123
+              </Typography>
             </Stack>
-            <Typography variant="h2" sx={{ textAlign: "center" }}>
-              Test text qwe qwe qwe qwe 123 123 123 123 123 123 123
-            </Typography>
-          </Stack>
-        </Paper>
+          </Paper>
+        </Box>
       </Container>
     </div>
   )
